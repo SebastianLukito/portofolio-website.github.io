bd8b1db1fbaf2afcb792f41f93f95d7a24b10c28
SebastianLukito <113852181+SebastianLukito@users.noreply.github.com>
2024-04-13 00:55:15 +0700

    first commit

---
+++ assets/js/hobby.js
@@ -0,0 +1,86 @@
+const prevBtn = document.querySelector('#prev-btn');
+const nextBtn = document.querySelector('#next-btn');
+const book = document.querySelector('#book');
+
+const paper1 = document.querySelector('#p1')
+const paper2 = document.querySelector('#p2')
+const paper3 = document.querySelector('#p3')
+
+prevBtn.addEventListener("click", goPrevious);
+nextBtn.addEventListener("click", goNext);
+
+let currentState = 1;
+let numOfPapers = 3;
+let maxState = numOfPapers + 1;
+
+
+function openBook() {
+    book.style.transform = "translateX(50%)";
+    prevBtn.style.transform = "translateX(-300px)";
+    nextBtn.style.transform = "translateX(300px)";
+}
+
+function closeBook(isFirstPage) {
+    if(isFirstPage) {
+        book.style.transform = "translateX(0%)";
+    } else {
+        book.style.transform = "translateX(100%)";
+    }
+    prevBtn.style.transform = "translateX(0px)";
+    nextBtn.style.transform = "translateX(0px)";
+}
+
+prevBtn.style.display = "none";
+
+function goNext() {
+    if(currentState < maxState) { 
+        switch(currentState) {
+            case 1:
+                openBook();
+                paper1.classList.add("flipped");
+                paper1.style.zIndex = 1;
+                prevBtn.style.display = "block";
+                break;
+            case 2:
+                paper2.classList.add("flipped");
+                paper2.style.zIndex = 2;
+                break;
+            case 3:
+                closeBook(false);
+                paper3.classList.add("flipped");
+                paper3.style.zIndex = 3;
+                nextBtn.style.display = "none";
+                break;
+            default: 
+                throw new Error("unkown state");    
+        }
+
+        currentState++;
+    }
+}
+
+function goPrevious() {
+    if(currentState > 1) {
+        switch(currentState) {
+            case 2:
+                closeBook(true);
+                paper1.classList.remove("flipped");
+                paper1.style.zIndex = 3;
+                prevBtn.style.display = "none";
+                break;
+            case 3:
+                paper2.classList.remove("flipped");
+                paper2.style.zIndex = 2;
+                break;
+            case 4: 
+                openBook()
+                paper3.classList.remove("flipped");
+                paper3.style.zIndex = 1;
+                nextBtn.style.display = "block";
+                break;
+        }
+
+        currentState--;
+    }
+}
+
